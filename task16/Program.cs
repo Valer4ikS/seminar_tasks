ff092046807276c6b4465fdc088f47971a812c9f
Valer4ikS <maraclee@yandex.ru>
2023-03-21 01:18:24 +0300

    add 17, 18

---
+++ task16/Program.cs
@@ -0,0 +1,25 @@
+﻿/* Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+0, 7, 8, -2, -2 -> 2
+
+1, -7, 567, 89, 223-> 3 */
+
+// Console.Write("Введите число M: ");
+// int m = int.Parse(Console.ReadLine()!);
+// int[] array = new int[m];
+
+// int CountPol(int[] size)
+// {
+//     int count = 0;
+//     foreach( int el in array)
+//     {
+//         if(el > 0) count ++;
+//     }
+//     return count;
+// }
+
+// CountPol(array);
+// Console.WriteLine(String.Join(", ", array));
+// Console.WriteLine($"{CountPol(array)}");
+
+
---
+++ task17/Program.cs
@@ -0,0 +1,48 @@
+﻿/* Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+0, 7, 8, -2, -2 -> 2
+
+1, -7, 567, 89, 223-> 3 */
+
+
+int Promt(string message)
+{
+    Console.Write(message);
+    string value = Console.ReadLine()!;
+    int num = Convert.ToInt32(value);
+    return num;
+}
+
+int[] GetArray(int length)
+{
+    int[] array = new int[length];
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = Promt($"Введите {i + 1} число: ");
+    }
+    return array;
+}
+
+void PrintArray(int[] array)
+{
+    Console.WriteLine($"[{string.Join(", ", array)}]");
+}
+
+int CountAboveZero(int[] m)
+{
+    int count = 0;
+    
+    for (int i = 0; i < m.Length; i++)
+    {
+        if (m[i] > 0)
+            count++;
+    }
+    return count;
+}
+
+int count = Promt("Кол-во чисел: ");
+int[] array;
+array = GetArray(count);
+PrintArray(array);
+Console.WriteLine($"{CountAboveZero(array)}");
+
